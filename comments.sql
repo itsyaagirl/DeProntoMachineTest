844da8ee05084278d242b4f7a9a4730e29db3c6d
Reva Kaneri <90758315+itsyaagirl@users.noreply.github.com>
2022-09-11 21:12:55 +0530

    initial commit

---
+++ comments.sql
@@ -0,0 +1,12 @@
+/*Sample Code not linked with the webpages but this is how I will display and
+save comments made by users, form a previous project*/
+$id = $_GET['threadid'];
+    $sql = "SELECT * FROM `comments` WHERE thread_id=$id";
+    $result = mysqli_query($conn, $sql);
+    $noResult = true;
+    while($row = mysqli_fetch_assoc($result)){
+        $noResult = false;
+        $id = $row['comment_id'];
+        $content = $row['comment_content'];
+        $comment_time = $row['comment_time'];
+        $thread_user_id = $row['comment_by'];
